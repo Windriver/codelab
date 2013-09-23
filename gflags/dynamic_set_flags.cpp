a0fed70bf23ed4a1abb698292c51428c36dba737
windriver <windriver@home-pc.vm>
2013-09-24 02:32:04 +0800

    dynamic set glags

---
+++ gflags/dynamic_set_flags.cpp
@@ -0,0 +1,24 @@
+// Copyright 2013, ZhongYi Inc.
+// Author: Windriver <windriver1986@gmail.com>
+
+#include <stdlib.h>
+
+#include "thirdparty/gflags/gflags.h"
+#include "thirdparty/glog/logging.h"
+
+DEFINE_string(test_str, "initial", "");
+DEFINE_int32(test_int32, -1, "");
+
+int main(int argc, char *argv[]) {
+    LOG(INFO) << "Dynamic set string gflag...";
+    LOG(INFO) << "Before set: " << FLAGS_test_str;
+    LOG(INFO) << google::SetCommandLineOption("test_str", "zhongyi");
+    LOG(INFO) << "After set: " << FLAGS_test_str;
+
+    LOG(INFO) << "Dynamic set int32 gflag...";
+    LOG(INFO) << "Before set: " << FLAGS_test_int32;
+    LOG(INFO) << google::SetCommandLineOption("test_int32", "15191430");
+    LOG(INFO) << "After set: " << FLAGS_test_int32;
+
+    return EXIT_SUCCESS;
+}
