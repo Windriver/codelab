2ff371e40e0e836f5a2a6857689f6f0c00347c18
Windriver <zy081314@qq.com>
2013-09-25 13:03:59 +0800

    visit evernote of oneself

--- evernote/visit_my_evernote.py
+++ evernote/visit_my_evernote.py
@@ -1,38 +1,38 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright 2013, ZhongYi Inc.
 # Author: Windriver <windriver1986@gmail.com>
 
-import hashlib
-import binascii
-import evernote.edam.userstore.constants as UserStoreConstants
-import evernote.edam.type.ttypes as Types
-
-from evernote.api.client import EvernoteClient
-from evernote.api.client import Store
 from evernote.edam.userstore import UserStore
+from evernote.edam.notestore import NoteStore
+
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
 
 
-dev_token = "S=s9:U=f38d5:E=148a8cf4838:C=141511e1c3a:P=1cd:A=en-devtoken:V=2:H=06d5f3f2d0f723c0160290955387d55a"
-store_url = "https://app.yinxiang.com/shard/s9/notestore"
-#client = EvernoteClient(token=dev_token)
-#userStore = client.get_user_store()
-#user = userStore.getUser()
-##print user.username
+print "代码演示了如何调用中文Evernote（印象笔记）的API来访问开发者自己的笔记。主要难点是理解Thrift RPC的使用，以及UserStore和NoteStore这两个核心概念"
 
-#client =  Store(dev_token, EvernoteClient, store_url)
+# 开发者Token和note_store_url使用你从官方获得的（链接：http://dev.yinxiang.com/documentation/cloud/chapters/Authentication.php#devtoken）
+# user_store_url是通用的
+dev_token = "Fill it with your own token"
+user_store_url = "https://app.yinxiang.com/edam/user"
+note_store_url = "https://app.yinxiang.com/shard/s9/notestore"
 
-# 获取印象笔记 NoteStore URL
+# 建立 UserStore 的 Client
+user_store_client = THttpClient.THttpClient(user_store_url)
+user_store_proto = TBinaryProtocol.TBinaryProtocol(user_store_client)
+user_store = UserStore.Client(user_store_proto, user_store_proto)
 
-userStoreProt = TBinaryProtocol.TBinaryProtocol(THttpClient.THttpClient(store_url));
-userStore = UserStore.Client(userStoreProt, userStoreProt);
-notestoreUrl = userStore.getNoteStoreUrl(dev_token);
+print "\n输出用户的信息:"
+user = user_store.getUser(dev_token)
+print user.username, user.id
 
-# 建立 NoteStore client 
-noteStoreTrans = THttpClient.THttpClient(notestoreUrl);
-noteStoreTrans.setCustomHeader("User-Agent", myUserAgent);
-noteStoreProt = TBinaryProtocol(noteStoreTrans);
-noteStore = NoteStore.Client(noteStoreProt, noteStoreProt);
+# 建立 NoteStore 的 Client 
+note_store_client = THttpClient.THttpClient(note_store_url)
+note_store_proto = TBinaryProtocol.TBinaryProtocol(note_store_client)
+note_store = NoteStore.Client(note_store_proto, note_store_proto)
 
+print "\n输出各个笔记本的信息:"
+notebooks = note_store.listNotebooks(dev_token)
+for notebook in notebooks:
+  print notebook.name, notebook.serviceCreated
